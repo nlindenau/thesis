781a178a4a7f81a99345ef8e1a50c50ffaa76437
Nicole Lindenau <nikoleta.lindenau@gmail.com>
2023-09-21 20:39:02 +0300

    ci/add-tests (#9)
    * ci: add github prebuild github action running pytest
    
    * ci: limit runs to on PR
    
    * docs: add ci badge

--- src/get_nutritional_facts.py
+++ src/get_nutritional_facts.py
@@ -1,4 +1,9 @@
-EXAMPLE_LABEL = {"calories": 100, "fat": 9, "saturated_fat": 1, "sodium": 7, "protein": 4}
+EXAMPLE_LABEL = {"calories": 100, 
+                 "fat": 9, 
+                 "saturated_fat": 1, 
+                 "sodium": 7, 
+                 "protein": 4, 
+                 }
 
 def get_nutritional_facts():
     return EXAMPLE_LABEL
