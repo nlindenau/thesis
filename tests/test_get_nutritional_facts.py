916124e9623f71ec8a0665b10fd95e219311cdbe
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2023-09-21 20:28:47 +0300

    chore: apply lint suggestions

--- tests/test_get_nutritional_facts.py
+++ tests/test_get_nutritional_facts.py
@@ -2,4 +2,8 @@ from src.get_nutritional_facts import get_nutritional_facts
 
 def test_nutritional_facts():
     result = get_nutritional_facts()
-    assert result == {"calories": 100, "fat": 9, "saturated_fat": 1, "sodium": 7, "protein": 4}
+    assert result == {"calories": 100, 
+                      "fat": 9, 
+                      "saturated_fat": 1, 
+                      "sodium": 7, 
+                      "protein": 4}
