114ef0b01ffff56d819fb451598d14d56fc99448
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2023-09-22 16:16:29 +0300

    refactor: import constants instead of duplicating

--- src/app.py
+++ src/app.py
@@ -1,17 +1,8 @@
-from calculate_nutritional_value import create_nutritional_label_for_ingredient, display_nutritional_label
-
-EXAMPLE_APPLE_NUTRITIONAL_FACTS_PER_100G = {"calories": 52,
-                                   "fat": 0.17,
-                                   "saturated_fat":0.028,
-                                   "carbohydrates":13.8,
-                                   "sugars":10.4,
-                                   "sodium":1,
-                                   "protein":0.26,
-                                   }
-
+from calculate_nutritional_values import create_nutritional_label_for_ingredient, display_nutritional_label
+from example_values import APPLE_NUTRITIONAL_FACTS_PER_100G, CREAM_CHEESE_NUTRITIONAL_FACTS_PER_100G, HONEY_NUTRITIONAL_FACTS_PER_100G
 
 def main():
-    my_label = create_nutritional_label_for_ingredient(600, EXAMPLE_APPLE_NUTRITIONAL_FACTS_PER_100G)
+    my_label = create_nutritional_label_for_ingredient(600, APPLE_NUTRITIONAL_FACTS_PER_100G)
     display_nutritional_label(my_label)
     print(my_label)
 
