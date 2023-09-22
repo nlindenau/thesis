bc082060ea4db78b7286209b943a476a451e8743
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2023-09-22 16:13:41 +0300

    feat: create nutritional label generator

--- src/calculate_nutritional_value.py
+++ src/calculate_nutritional_value.py
@@ -1,5 +1,21 @@
 def get_nutritional_value(serving, nutritional_value_per_100g):
     '''Takes in size of the serving of the ingredient and 
     a nutritional value (eg. calories) per 100 g.'''
-    nutritional_value = nutritional_value_per_100g * (serving / 100)
+    nutritional_value = round(nutritional_value_per_100g * (serving / 100), 2)
     return nutritional_value
+
+def create_nutritional_label_for_ingredient(serving, nutritional_values_per_100):
+    '''Takes in size of the serving of the ingredient and 
+    a dictionary of nutritional facts per 100g, 
+    returns a dictionary of nutritional values for the entire serving.'''
+    nutritional_label = {}
+    for nutrient in nutritional_values_per_100:
+        nutrient_amount_per_100g = nutritional_values_per_100[nutrient]
+        nutrient_amount_per_serving = get_nutritional_value(serving, nutrient_amount_per_100g)
+        nutritional_label.update({nutrient: nutrient_amount_per_serving})
+
+    return nutritional_label
+
+def display_nutritional_label(nutritional_label):
+    for nutrient in nutritional_label:
+        print(nutrient, nutritional_label[nutrient])
--- tests/test_calculate_nutritional_value.py
+++ tests/test_calculate_nutritional_value.py
@@ -1,5 +1,17 @@
-from src.calculate_nutritional_value import get_nutritional_value
+from src.calculate_nutritional_value import get_nutritional_value, create_nutritional_label_for_ingredient
+from src.example_values import APPLE_NUTRITIONAL_FACTS_PER_100G
 
-def test_nutritional_value():
+def test_get_nutritional_value():
     result = get_nutritional_value(serving=600, nutritional_value_per_100g=100)
     assert result == 600
+
+def test_create_nutritional_label_for_ingredient():
+    result = create_nutritional_label_for_ingredient(serving=600, 
+                                                     nutritional_values_per_100g=APPLE_NUTRITIONAL_FACTS_PER_100G)
+    assert result == {'calories': 312.0, 
+                      'fat': 1.02, 
+                      'saturated_fat': 0.17, 
+                      'carbohydrates': 82.8, 
+                      'sugars': 62.4, 
+                      'sodium': 6.0, 
+                      'protein': 1.56}
