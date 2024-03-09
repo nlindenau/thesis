4b33c9c3016afda45ec5a5098010f4c240db4129
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2024-03-09 21:30:24 +0200

    test: add dummy workflow in app

--- src/app.py
+++ src/app.py
@@ -4,15 +4,23 @@ from fuzzy_match_fineli_response import get_names_similarity_score, get_most_sim
 
 import pprint
 
-def main():
-    query = "Honey, raw"
-    fineli_response = get_data_from_fineli("Honey, raw")
+def fineli_workflow(query):
+    fineli_response = get_data_from_fineli(query)
     clean_fineli_response = get_list_of_nutrition_values(fineli_response)
-    list_with_similarity_scores = get_names_similarity_score(clean_fineli_response, query)
-    most_similar_result = get_most_similar_entry(list_with_similarity_scores)
-    print(type(most_similar_result))
+    return clean_fineli_response
 
-    print(most_similar_result)
+def get_best_matching_entry(cleaned_fineli_response, query):
+    list_with_similarity_scores = get_names_similarity_score(cleaned_fineli_response, query)
+    most_similar_result = get_most_similar_entry(list_with_similarity_scores)
+    return most_similar_result
+
+def main():
+    query = "Strawberry jam"
+    fineli_response = fineli_workflow(query)
+    most_similar_response = get_best_matching_entry(fineli_response, query)
+    print(type(most_similar_response))
+
+    print(most_similar_response)
 
 
 main()
