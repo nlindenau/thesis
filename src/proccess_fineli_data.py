3e29a567a09e55710be567a8de805898d9035604
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2024-03-09 21:17:13 +0200

    feat: add data wrangling to clean up Fineli response

--- src/proccess_fineli_data.py
+++ src/proccess_fineli_data.py
@@ -10,7 +10,7 @@ def print_all_food_names(fineli_response, language="en"):
         print(element["id"], "-" ,element["name"][language])
 
 def get_list_of_nutrition_values(fineli_response, language="en"):
-    '''Clean up Fineli response to get only the EU-law required information for nutrition declaration.
+    '''Clean up Fineli response to get only the EU-law required information for nutrition declaration per item.
 
     Keyword arguments:
     fineli_response -- JSON of the Fineli API response.
