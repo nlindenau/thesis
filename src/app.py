64c2ccee6ea21982e91c8ed7b2dfd097150fca9d
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2024-03-17 23:09:04 +0200

    feature: add flask api endpoints

--- src/app.py
+++ src/app.py
@@ -1,41 +1,31 @@
-from process_user_request import get_list_of_ingredients, get_ingredients_facts_from_fineli, calculate_nutiritonal_values_for_servings, sum_nutritional_values_for_all_ingredients, create_full_response
+from flask import Flask, request, jsonify
+from src.process_user_request import get_list_of_ingredients, get_ingredients_facts_from_fineli, calculate_nutiritonal_values_for_servings, sum_nutritional_values_for_all_ingredients, create_full_response
 
+app = Flask(__name__)
 
-def main():
-    user_request = [
-    {
-        "name": "Apple, average",
-        "weight": 300
-    },
-    {
-        "name": "Cream Cheese, low-fat",
-        "weight": 50
-    },
-    {
-        "name": "Honey, raw",
-        "weight": 10
-    },
-]
-    ingredients = get_list_of_ingredients(user_request)
+EXAMPLE_RETURN = {"message": "hello"}
+@app.route("/")
+def welcome_message():
+    return "<p>Hello from RVAL.</p>"
 
-    #cleaned_fineli_response = run_fineli_workflow(query)
-    #most_similar_entry = run_fuzzy_matching_workflow(cleaned_fineli_response, query)
-    #print(type(most_similar_entry))
+@app.post("/api/v1/nutritionfacts")
+def nutrition_facts_calculator():
+    request_body = request.json 
+    request_body = sorted(request_body, key=lambda d: d['name'])
 
-    #print(most_similar_entry)
+    ingredients = get_list_of_ingredients(request_body)
+    ingredients.sort()
 
     fineli_list = get_ingredients_facts_from_fineli(ingredients)
+    fineli_list = sorted(fineli_list, key=lambda d: d['name'])
 
     fineli_ingredients = get_list_of_ingredients(fineli_list)
+    fineli_ingredients.sort()
 
-    print(fineli_list)
-
-    nutritional_values_for_servings = calculate_nutiritonal_values_for_servings(user_request, fineli_list)
+    nutritional_values_for_servings = calculate_nutiritonal_values_for_servings(request_body, fineli_list)
 
     final_label = sum_nutritional_values_for_all_ingredients(nutritional_values_for_servings)
 
     response = create_full_response(ingredients, fineli_ingredients, final_label)
 
-    print(response)
-
-main()
+    return jsonify(response)
