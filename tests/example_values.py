a2467d8ad0405db854b02feae3515d00e077dbc9
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2024-03-18 18:27:47 +0200

    test: readd example values

---
+++ tests/example_values.py
@@ -0,0 +1,30 @@
+
+#Source: FoodData Central 
+APPLE_NUTRITIONAL_FACTS_PER_100G = {"calories": 52,
+                                   "fat": 0.17,
+                                   "saturated_fat": 0.028,
+                                   "carbohydrates": 13.8,
+                                   "sugars": 10.4,
+                                   "sodium": 1,
+                                   "protein": 0.26,
+                                   }
+
+#Source: FoodData Central 
+CREAM_CHEESE_NUTRITIONAL_FACTS_PER_100G = {"calories": 343,
+                                   "fat": 33.5,
+                                   "saturated_fat": 19.7,
+                                   "carbohydrates": 4.56,
+                                   "sugars": 3.76,
+                                   "sodium": 368,
+                                   "protein": 5.79,
+                                   }
+
+#Source: FoodData Central 
+HONEY_NUTRITIONAL_FACTS_PER_100G = {"calories": 304,
+                                   "fat": 0,
+                                   "saturated_fat": 0,
+                                   "carbohydrates": 82.4,
+                                   "sugars": 82.12,
+                                   "sodium": 4,
+                                   "protein": 0.3,
+                                   }
