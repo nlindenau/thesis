c1c1536b954156ac643c4e3d1b7e528df75b9419
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2024-03-18 22:48:25 +0200

    refactor: add NutritionFactsLabel typed dict

---
+++ src/nutrition_facts.py
@@ -0,0 +1,11 @@
+from typing import TypedDict
+
+class NutritionFactsLabel(TypedDict):
+    "name": srt      
+    "energy": float
+    "fat": float
+    "saturatedFat": float
+    "carbohydrates": float
+    "sugars": float
+    "protein": float
+    "salt": float
