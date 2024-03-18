f77103f3bbf6cea3db45c8efb10e2dc9daa17f2d
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2024-03-18 22:50:56 +0200

    fix: change class definition

--- src/nutrition_facts.py
+++ src/nutrition_facts.py
@@ -1,11 +1,11 @@
 from typing import TypedDict
 
 class NutritionFactsLabel(TypedDict):
-    "name": srt      
-    "energy": float
-    "fat": float
-    "saturatedFat": float
-    "carbohydrates": float
-    "sugars": float
-    "protein": float
-    "salt": float
+    name: str   
+    energy: float
+    fat: float
+    saturatedFat: float
+    carbohydrates: float
+    sugars: float
+    protein: float
+    salt: float
