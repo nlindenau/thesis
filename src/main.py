585209f8073a0a963c5d6b351a80aa483dc38e67
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2024-03-20 19:39:03 +0200

    feat: add lambda handler

--- src/main.py
+++ src/main.py
@@ -1,26 +1,31 @@
 import flask
 import functions_framework
+import ast
+import json
 
-from process_user_request import get_list_of_ingredients, get_ingredients_facts_from_fineli, calculate_nutiritonal_values_for_servings, sum_nutritional_values_for_all_ingredients, create_full_response
+from process_user_request import run_request_processing_workflow
+
+def lambda_handler(event, context):
+
+    event_body = event.get("body") 
+
+    data = ast.literal_eval(event_body)
+
+    response_body = run_request_processing_workflow(data)
+    
+    response_body = json.dumps(response_body)
+
+    response = {"statusCode": 200,
+        "body": response_body}
+
+    return response
 
 @functions_framework.http
 def nutrition_facts_calculator(request: flask.Request) -> flask.typing.ResponseReturnValue:
     request_body = flask.request.json 
-    request_body = sorted(request_body, key=lambda d: d['name'])
 
-    ingredients = get_list_of_ingredients(request_body)
-    ingredients.sort()
+    response = run_request_processing_workflow(request_body)
 
-    fineli_list = get_ingredients_facts_from_fineli(ingredients)
-    fineli_list = sorted(fineli_list, key=lambda d: d['name'])
-
-    fineli_ingredients = get_list_of_ingredients(fineli_list)
-    fineli_ingredients.sort()
-
-    nutritional_values_for_servings = calculate_nutiritonal_values_for_servings(request_body, fineli_list)
-
-    final_label = sum_nutritional_values_for_all_ingredients(nutritional_values_for_servings)
-
-    response = create_full_response(ingredients, fineli_ingredients, final_label)
+    response = flask.jsonify(response)
 
     return response, 200
